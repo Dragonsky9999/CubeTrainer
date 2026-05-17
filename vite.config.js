fd29aa6940e0112207abc82c24eb5bdc542272eb
Dragonsky <riku.hukano@icloud.com>
2026-05-17 12:07:54 +0900

    adjust vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite'
 
-export default defineConfig(({ mode }) => {
-  return {
-    base: mode === 'production' ? '/CubeTrainer/' : './',
-  }
+export default defineConfig({
+  base: '/CubeTrainer/',
 })
